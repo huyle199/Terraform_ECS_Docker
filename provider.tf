1dfec54d30418c7b0990673129f7cf2ca5199ab8
Huy Le <86170240+huyle199@users.noreply.github.com>
2022-08-22 23:03:22 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,12 +11,10 @@ terraform {
   }
 }
 
-
-# Specifying the Docker provider configuration
-provider "docker" {
-  host = "unix:///var/run/docker.sock"
-}
+provider "docker" {}
 
 provider "aws" {
   region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
